ca101a0a7f5800c88174a19b51f570df79858696
Aram <aramvardanyan13@gmail.com>
2023-09-29 16:24:55 +0400

    4w 5d

---
+++ 4__week_C++/5__day_C++/1__2d_matrix_IS_simetric.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+bool Is_simetric(int arr[][3]);
+
+int main(){
+
+ const int SIZE = 3;
+ int arr[SIZE][SIZE];
+
+	for(int i = 0; i < SIZE; ++i){
+		for(int j = 0; j < SIZE; ++j){
+			std::cin >> arr[i][j];
+		}
+	}
+
+	if(Is_simetric(arr)){
+		std::cout << "SIMETRIC" << std::endl;	
+	}
+	else{
+		std::cout << "NO" << std::endl;	
+	}
+		
+}
+
+bool Is_simetric(int arr[][3]){
+
+	for(int i = 0; i < 3; ++i){
+		for(int j = 0; j < 3; ++j){
+//			if(arr[i][j] != arr[j][i] || (arr[i][i] != arr[3 - 1 - i][3 - 1 - i])){ // + ankjunagic
+			if(arr[i][j] != arr[j][i]){
+				return false;
+			}
+		}
+	}
+
+	return true;
+
+}
