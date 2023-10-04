5323a21173bdd78cd9e3a6af336912228751c43a
Aram <aramvardanyan13@gmail.com>
2023-10-04 15:45:35 +0400

    5w 2d

---
+++ 5__week_C++/2__day_C++/5__num_without_dublicate.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int find(int* arr, int SIZE);
+
+int main(){
+
+ const int SIZE = 7;
+ int arr[SIZE] = {1, 2, 3, 2, 1, 5, 5};
+
+ std::cout << find(arr, SIZE) << std::endl; 
+}
+
+int find(int* arr, int SIZE){
+	int num = 0;
+
+	for(int i = 0; i < SIZE; ++i){
+		num ^= arr[i];
+	}
+
+	return num;
+}
