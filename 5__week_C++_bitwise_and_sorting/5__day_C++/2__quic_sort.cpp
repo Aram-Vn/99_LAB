ccb56ad34f52215b1b0dfb32d342e5d4ff4ff0c8
Aram <aramvardanyan13@gmail.com>
2023-10-10 13:54:54 +0400

    5w 5d

--- 5__week_C++_bitwise_and_sorting/5__day_C++/2__quic_sort.cpp
+++ 5__week_C++_bitwise_and_sorting/5__day_C++/2__quic_sort.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 
-int quick_sort(int* arr, int start, int end);
+int quick_sort_helper(int* arr, int start, int end);
+void quick_sort(int* arr, int start, int end);
 
 int main(){
 
- const int SIZE = 6;
+ const int SIZE = 8;
  int arr[SIZE];
 
 	for(int i = 0; i < SIZE; ++i){
@@ -14,7 +15,7 @@ int main(){
  int start = 0;
  int end = SIZE - 1;
 
- std::cout << quick_sort(arr, start, end) << std::endl;;
+ quick_sort(arr, start, end);
 
 	for(int i = 0; i < SIZE; ++i){
 		std::cout << arr[i] << " ";
@@ -23,16 +24,26 @@ int main(){
 	
 }
 
-int quick_sort(int* arr, int start, int end){
+int quick_sort_helper(int* arr, int start, int end){
 	int pivot_ind = start - 1;
 
-	for(int i = 0; i <= end; ++i){
-		if(arr[i] <= arr[end] ){
+	for(int i = start; i <= end - 1; ++i){
+		if(arr[i] < arr[end] ){
 			++pivot_ind;
 			std::swap(arr[pivot_ind], arr[i]);	
 		}
 	}
+	std::swap(arr[pivot_ind + 1], arr[end]);	
 
-	return pivot_ind;
+	return pivot_ind + 1;
 }
 
+void quick_sort(int* arr, int start, int end){
+
+	if(start < end){
+		int pivot = quick_sort_helper(arr, start, end);
+		quick_sort(arr, start, pivot - 1);
+		quick_sort(arr, pivot + 1, end);
+	}
+
+}
