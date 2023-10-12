9ef83e836377db54ed6ce6a0b620d20d54636f9f
Aram <aramvardanyan13@gmail.com>
2023-10-12 14:22:15 +0400

    6w 2d

---
+++ 6__week_C++_working_with_files/2__day_C++/7__Caesar_cipher_Decoder.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <fstream>
+
+void Caesar_cipher(std::string& str, int kee);
+
+int main(){
+
+ int kee = 0;
+ std::cout << "enter kee" << std::endl;
+ std::cin >> kee;
+
+ std::ifstream reader;
+
+ reader.open("alp.result.txt"); 
+	
+	if(!reader.is_open()){
+		std::cout << "File si not open" << std::endl;
+		return -1;	
+	}
+
+ std::string str;
+
+ std::ofstream writer;
+
+ writer.open("alp.decoded.txt");
+
+	if(!writer.is_open()){
+		std::cout << "File si not open" << std::endl;
+		return -1;	
+	}
+
+	while(!reader.eof()){
+		std::getline(reader, str);
+		if(str == ""){
+			continue;
+		}
+		Caesar_cipher(str, kee);
+		writer << str << std::endl;
+	}
+
+	if(reader.is_open()){
+		reader.close();
+	}
+
+	if(reader.is_open()){
+		reader.close();
+	}
+
+}
+
+
+void Caesar_cipher(std::string& str, int kee){
+	for(int i = 0; i < str.length(); ++i){
+		if(str[i] >= 'a' && str[i] <= 'z'){ 
+			str[i] = (str[i] - 'a' - kee + 26) % 26 + 'a';
+		}
+		else if(str[i] >= 'A' && str[i] <= 'Z'){
+			str[i] = (str[i] - 'A' - kee + 26) % 26 + 'A';
+		} 
+	}
+}
